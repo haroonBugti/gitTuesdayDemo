0693fca3c04ccdae0cd43090ad615bcf7e069389
haroon <m.haroon>
2022-06-07 14:38:59 +0500

    for temporary only

---
+++ tuesdayDemoPurpose/tuesdayDemoPurpose/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  tuesdayDemoPurpose
+//
+//  Created by haroon mazhar on 07/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        ali()
+    }
+    func ali(){
+        print("hi")
+    }
+
+}
+
